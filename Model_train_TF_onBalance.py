5f16af7778ad00d7da888f5564b24d5c7d7c0919
Leci37 <leciluis@gmail.com>
2022-11-08 00:23:35 +0100

    call to ModelDefinition: ModelDefinition(shape_inputs_m=imput_shape, num_features_m=len(df.columns) )

--- Model_train_TF_onBalance.py
+++ Model_train_TF_onBalance.py
@@ -3,6 +3,7 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 
+from Model_TF_definitions import ModelDefinition
 from Utils import UtilsL, Utils_model_predict, Utils_plotter
 import a_manage_stocks_dict
 
@@ -12,6 +13,8 @@ import a_manage_stocks_dict
 #     patience=9,
 #     mode='auto',#min_delta=1 By default, any change in the performance measure, no matter how fractional, will be considered an improvement
 #     restore_best_weights=True)
+from Utils.Utils_model_predict import __print_csv_accuracy_loss_models
+
 
 def get_EarlyStopping(model_h5_name):
     monitor_type = 'val_accuracy'#EarlyStopping has 4 values: 'loss','accuracy','val_loss','val_accuracy'.
@@ -47,7 +50,7 @@ MODEL_FOLDER_TF = "Models/TF_balance/"
 #train_labels, val_labels, test_labels, train_features, val_features, test_features, bool_train_labels = None
 
 
-def train_TF_onBalance(columns_selection  , model_h5_name   , path_csv , op_buy_sell : a_manage_stocks_dict.Op_buy_sell):
+def train_TF_onBalance_One_dimension(columns_selection  , model_h5_name   , path_csv , op_buy_sell : a_manage_stocks_dict.Op_buy_sell, type_model_dime : a_manage_stocks_dict.MODEL_TF_DENSE_TYPE_ONE_DIMENSI):
     #LOAD
     global train_labels, val_labels, test_labels, train_features, val_features, test_features, bool_train_labels
     df = Utils_model_predict.load_and_clean_DF_Train_from_csv(path_csv, op_buy_sell, columns_selection)
@@ -62,13 +65,13 @@ def train_TF_onBalance(columns_selection  , model_h5_name   , path_csv , op_buy_
     # TRAIN
     neg, pos = np.bincount(df[Y_TARGET])
     initial_bias = np.log([pos / neg])
-    model = Utils_model_predict.make_model_TF_onbalance_fine_28(shape_features=train_features.shape[-1])
+    model = ModelDefinition(shape_inputs_m=train_features.shape[-1], num_features_m=None).get_dicts_models_One_dimension()[type_model_dime]
     print(model.summary())
     results = model.evaluate(train_features, train_labels, batch_size=BATCH_SIZE, verbose=2)
     print("Loss: {:0.4f}  without output_bias ".format(results[0]))
     # model.predict(train_features[:10])
-    model = Utils_model_predict.make_model_TF_onbalance_fine_28(shape_features=train_features.shape[-1],
-                                                                output_bias=initial_bias)
+    model = ModelDefinition(shape_inputs_m=train_features.shape[-1], num_features_m=None, output_bias_m= initial_bias).get_dicts_models_One_dimension()[type_model_dime]
+
     results = model.evaluate(train_features, train_labels, batch_size=BATCH_SIZE, verbose=2)
     print("Loss: {:0.4f} with output_bias".format(results[0]))
     # model.predict(train_features[:10])
@@ -81,7 +84,7 @@ def train_TF_onBalance(columns_selection  , model_h5_name   , path_csv , op_buy_
     # Train on the oversampled data
     # Now try training the model with the resampled data set instead of using class weights to see how these methods compare.
     # Note: Because the data was balanced by replicating the positive examples, the total dataset size is larger, and each epoch runs for more training steps.
-    resampled_model = Utils_model_predict.make_model_TF_onbalance_fine_28(shape_features=train_features.shape[-1])
+    resampled_model = ModelDefinition(shape_inputs_m=train_features.shape[-1], num_features_m=None).get_dicts_models_One_dimension()[type_model_dime]
     resampled_model.load_weights(initial_weights)
     # Reset the bias to zero, since this dataset is balanced.
     output_layer = resampled_model.layers[-1]
@@ -100,68 +103,68 @@ def train_TF_onBalance(columns_selection  , model_h5_name   , path_csv , op_buy_
         callbacks=[early_stopping], #callbacks=[early_stopping, early_stopping_board],
         validation_data=val_ds)
 
-    __print_csv_accuracy_loss_models(model_h5_name, resampled_history)
+    __print_csv_accuracy_loss_models(MODEL_FOLDER_TF, model_h5_name, resampled_history)
     resampled_model.save(MODEL_FOLDER_TF + model_h5_name)
     print(" Save model :  ", MODEL_FOLDER_TF + model_h5_name)
 
 
 
 
-def train_TF_onBalance_64(columns_selection, model_h5_name,path_csv, op_buy_sell : a_manage_stocks_dict.Op_buy_sell):
-    # LOAD
-    global train_labels, val_labels, test_labels, train_features, val_features, test_features, bool_train_labels
-    df = Utils_model_predict.load_and_clean_DF_Train_from_csv(path_csv, op_buy_sell, columns_selection)
-
-    # graficos de relaciones
-    # Utils_plotter.plot_relationdist_main_val_and_all_rest_val(df[["mtum_RSI","mtum_STOCH_k","mtum_STOCH_d", Y_TARGET]],Y_TARGET ,path = model_folder+"plot_relationdistplot_")
-    train_labels, val_labels, test_labels, train_features, val_features, test_features, bool_train_labels = Utils_model_predict.scaler_split_TF_onbalance(
-        df, label_name=Y_TARGET)
-    # END LOAD
-
-    # TRAIN
-    neg, pos = np.bincount(df[Y_TARGET])
-    initial_bias = np.log([pos / neg])
-    model = Utils_model_predict.make_model_TF_onbalance_fine_64(shape_features=train_features.shape[-1])
-    print(model.summary())
-    results = model.evaluate(train_features, train_labels, batch_size=BATCH_SIZE, verbose=2)
-    print("Loss: {:0.4f}  without output_bias ".format(results[0]))
-    # model.predict(train_features[:10])
-    model = Utils_model_predict.make_model_TF_onbalance_fine_64(shape_features=train_features.shape[-1],
-                                                                output_bias=initial_bias)
-    results = model.evaluate(train_features, train_labels, batch_size=BATCH_SIZE, verbose=2)
-    print("Loss: {:0.4f} with output_bias".format(results[0]))
-    # model.predict(train_features[:10])
-    initial_weights = MODEL_FOLDER_TF + "initial_weights/initial_weights_" + model_h5_name
-    model.save_weights(initial_weights)
-    print("model.save_weights initial_weights: ", initial_weights)
-    resampled_ds = Utils_model_predict.get_resampled_ds_onBalance(train_features, train_labels, bool_train_labels,
-                                                                  BATCH_SIZE)
-    resampled_steps_per_epoch = np.ceil(2.0 * neg / BATCH_SIZE)
-    # Train on the oversampled data
-    # Now try training the model with the resampled data set instead of using class weights to see how these methods compare.
-    # Note: Because the data was balanced by replicating the positive examples, the total dataset size is larger, and each epoch runs for more training steps.
-    resampled_model = Utils_model_predict.make_model_TF_onbalance_fine_64(shape_features=train_features.shape[-1])
-    resampled_model.load_weights(initial_weights)
-    # Reset the bias to zero, since this dataset is balanced.
-    output_layer = resampled_model.layers[-1]
-    output_layer.bias.assign([0])
-    val_ds = tf.data.Dataset.from_tensor_slices((val_features, val_labels)).cache()
-    val_ds = val_ds.batch(BATCH_SIZE).prefetch(2)
-
-    #early_stopping = get_EarlyStopping(model_h5_name)
-    early_stopping = Utils_model_predict.CustomEarlyStopping(patience=10)
-    early_stopping_board = get_EarlyStopping_TensorFlowBoard(model_h5_name)
-
-    resampled_history = resampled_model.fit(
-        resampled_ds,
-        epochs=EPOCHS,
-        steps_per_epoch=resampled_steps_per_epoch,
-        callbacks=[early_stopping], # callbacks=[early_stopping, early_stopping_board],
-        validation_data=val_ds)
-
-    __print_csv_accuracy_loss_models(model_h5_name, resampled_history)
-    resampled_model.save(MODEL_FOLDER_TF + model_h5_name)
-    print(" Save model :  ", MODEL_FOLDER_TF + model_h5_name)
+# def train_TF_onBalance_64(columns_selection, model_h5_name,path_csv, op_buy_sell : a_manage_stocks_dict.Op_buy_sell):
+#     # LOAD
+#     global train_labels, val_labels, test_labels, train_features, val_features, test_features, bool_train_labels
+#     df = Utils_model_predict.load_and_clean_DF_Train_from_csv(path_csv, op_buy_sell, columns_selection)
+#
+#     # graficos de relaciones
+#     # Utils_plotter.plot_relationdist_main_val_and_all_rest_val(df[["mtum_RSI","mtum_STOCH_k","mtum_STOCH_d", Y_TARGET]],Y_TARGET ,path = model_folder+"plot_relationdistplot_")
+#     train_labels, val_labels, test_labels, train_features, val_features, test_features, bool_train_labels = Utils_model_predict.scaler_split_TF_onbalance(
+#         df, label_name=Y_TARGET)
+#     # END LOAD
+#
+#     # TRAIN
+#     neg, pos = np.bincount(df[Y_TARGET])
+#     initial_bias = np.log([pos / neg])
+#     model = Utils_model_predict.make_model_TF_onbalance_fine_64(shape_features=train_features.shape[-1])
+#     print(model.summary())
+#     results = model.evaluate(train_features, train_labels, batch_size=BATCH_SIZE, verbose=2)
+#     print("Loss: {:0.4f}  without output_bias ".format(results[0]))
+#     # model.predict(train_features[:10])
+#     model = Utils_model_predict.make_model_TF_onbalance_fine_64(shape_features=train_features.shape[-1],
+#                                                                 output_bias=initial_bias)
+#     results = model.evaluate(train_features, train_labels, batch_size=BATCH_SIZE, verbose=2)
+#     print("Loss: {:0.4f} with output_bias".format(results[0]))
+#     # model.predict(train_features[:10])
+#     initial_weights = MODEL_FOLDER_TF + "initial_weights/initial_weights_" + model_h5_name
+#     model.save_weights(initial_weights)
+#     print("model.save_weights initial_weights: ", initial_weights)
+#     resampled_ds = Utils_model_predict.get_resampled_ds_onBalance(train_features, train_labels, bool_train_labels,
+#                                                                   BATCH_SIZE)
+#     resampled_steps_per_epoch = np.ceil(2.0 * neg / BATCH_SIZE)
+#     # Train on the oversampled data
+#     # Now try training the model with the resampled data set instead of using class weights to see how these methods compare.
+#     # Note: Because the data was balanced by replicating the positive examples, the total dataset size is larger, and each epoch runs for more training steps.
+#     resampled_model = Utils_model_predict.make_model_TF_onbalance_fine_64(shape_features=train_features.shape[-1])
+#     resampled_model.load_weights(initial_weights)
+#     # Reset the bias to zero, since this dataset is balanced.
+#     output_layer = resampled_model.layers[-1]
+#     output_layer.bias.assign([0])
+#     val_ds = tf.data.Dataset.from_tensor_slices((val_features, val_labels)).cache()
+#     val_ds = val_ds.batch(BATCH_SIZE).prefetch(2)
+#
+#     #early_stopping = get_EarlyStopping(model_h5_name)
+#     early_stopping = Utils_model_predict.CustomEarlyStopping(patience=10)
+#     early_stopping_board = get_EarlyStopping_TensorFlowBoard(model_h5_name)
+#
+#     resampled_history = resampled_model.fit(
+#         resampled_ds,
+#         epochs=EPOCHS,
+#         steps_per_epoch=resampled_steps_per_epoch,
+#         callbacks=[early_stopping], # callbacks=[early_stopping, early_stopping_board],
+#         validation_data=val_ds)
+#
+#     __print_csv_accuracy_loss_models(MODEL_FOLDER_TF, model_h5_name, resampled_history)
+#     resampled_model.save(MODEL_FOLDER_TF + model_h5_name)
+#     print(" Save model :  ", MODEL_FOLDER_TF + model_h5_name)
 
 
 def predict_TF_onBalance(X_test,  model_folder, model_h5_name):
@@ -184,22 +187,3 @@ def predict_TF_onBalance(X_test,  model_folder, model_h5_name):
     # Utils_plotter.plot_confusion_matrix(cf_matrix, model_folder + "plot_confusion_matrix.png")
     return  test_predictions_resampled
 
-
-def __print_csv_accuracy_loss_models(model_h5_name, resampled_history):
-    UtilsL.remove_files_starwith(MODEL_FOLDER_TF + model_h5_name + "_")
-    # resampled_history.model.metrics_names[1] # accuracy name
-    # resampled_history.history['accuracy'][-1]
-    # resampled_history.model.metrics_names[0] #Lost name
-    # resampled_history.history['loss'][-1]
-    # resampled_history.epoch[-1]
-    # resampled_history.params['epochs'] # 160 epos
-    data_hist_model = resampled_history.model.metrics_names[1] + "_" + "{:.2f}".format(
-        resampled_history.history['accuracy'][-1] * 100) + "%__" \
-                      + resampled_history.model.metrics_names[0] + "_" + "{:.2f}".format(
-        resampled_history.history['loss'][-1]) + "__" \
-                      + "epochs_" + str(resampled_history.epoch[-1]) + "[" + str(
-        resampled_history.params['epochs']) + "]"
-    pd.DataFrame(resampled_history.history).round(3).to_csv(
-        MODEL_FOLDER_TF + model_h5_name + "_" + data_hist_model + ".csv", sep="\t", index=None)
-
-#model_h5_name = 'TF_in_balance.h5'
