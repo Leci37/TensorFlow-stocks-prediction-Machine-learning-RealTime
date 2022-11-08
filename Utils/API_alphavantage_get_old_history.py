cc33f39d5dc09736da1f2299003417a39aead4c3
Leci37 <leciluis@gmail.com>
2022-11-09 00:31:31 +0100

    add # **DOCU**

--- Utils/API_alphavantage_get_old_history.py
+++ Utils/API_alphavantage_get_old_history.py
@@ -11,8 +11,8 @@ import pandas as pd
 # https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year20&adjusted=false&apikey=demo
 import a_manage_stocks_dict
 
-API_ALPHA_KEY = "BCOVDP6GILNXKZG9"
-API_LIST_ALPHA_KEY_2 = ["FXZ0","FXZ1","FXZ2","FXZ3","FXZ4","FXZ5","FXZ6","FXZ7","FXZ8","FXZ9","BCOVDP6GILNXKZG9", "NCLXRBHC77ABT2R7", "D29SLGXA2MSSL0EJ" , "2Q6DAQVVEHSDW9D2" , "YSZM7M3FA8EV632Q" , "T0BOX3F5S5BYFH8O", "PAQXEWTKRDJK4UGT", "713TN3DP9AXEC8QQ", "5DCMSRWZVJROU690", "4IYYMEPKRJSO7PCK", "0YVW8B4STUVL8ENF"]
+
+API_LIST_ALPHA_FREE_KEYS = ["FXZ0", "FXZ1", "FXZ2", "FXZ3", "FXZ4", "FXZ5", "FXZ6", "FXZ7", "FXZ8", "FXZ9", "BCOVDP6GILNXKZG9", "NCLXRBHC77ABT2R7", "D29SLGXA2MSSL0EJ" , "2Q6DAQVVEHSDW9D2" , "YSZM7M3FA8EV632Q" , "T0BOX3F5S5BYFH8O", "PAQXEWTKRDJK4UGT", "713TN3DP9AXEC8QQ", "5DCMSRWZVJROU690", "4IYYMEPKRJSO7PCK", "0YVW8B4STUVL8ENF"]
 ALL_TIME_OPTIONS = "year1month1", "year1month2", "year1month3", "year1month4", "year1month5", "year1month6", "year1month7", "year1month8", "year1month9",\
                    "year1month10", "year1month11", "year1month12", "year2month1", "year2month2", "year2month3", "year2month4", "year2month5", "year2month6", \
                    "year2month7", "year2month8", "year2month9", "year2month10", "year2month11", "year2month12"
@@ -23,23 +23,23 @@ S = "MELI"
 intelval = "15min"
 #time_opcion = "year2month12"
 
-CSV_NAME = "@CHIC"
-list_companys_FVOLA = a_manage_stocks_dict.DICT_COMPANYS[CSV_NAME]
+CSV_NAME = "@FOLO3"
+list_companys = a_manage_stocks_dict.DICT_COMPANYS[CSV_NAME]
 
-#El resto de una división se calcula en Python con el operador %
+#The remainder of a division is calculated in Python with the % operator
 count_int_try = 1
 
 
 def get_api_key():
     global count_int_try
-    resto = count_int_try % (len(API_LIST_ALPHA_KEY_2)) -1
+    resto = count_int_try % (len(API_LIST_ALPHA_FREE_KEYS)) - 1
     if resto == 0 and count_int_try != 1:
-        print("Excedidas las llamadas esperar 5min CountID: "+str(count_int_try))
+        print("Exceeded calls wait for 5min CountID: "+str(count_int_try))
         sleep(60)
-        key = API_LIST_ALPHA_KEY_2[resto]
+        key = API_LIST_ALPHA_FREE_KEYS[resto]
         count_int_try += 1
     else:
-        key = API_LIST_ALPHA_KEY_2[resto]
+        key = API_LIST_ALPHA_FREE_KEYS[resto]
     # print("obtenida key: "+key)
     return key
 
@@ -57,15 +57,23 @@ def do_request():
 def do_request_conunt_adder():
     global count_int_try
     count_int_try += 1
-    print("[1] Excedidas las llamadas , cambiar API_KEY Nueva: " )
+    print("[1] Exceeded calls, change API_KEY New:  " )
     raw_response_2 = do_request()
     if "for using Alpha Vantage! Our standard API call frequency is 5 calls per minute and 500 calls per day" in raw_response_2.text:
-        print("[2] Excedidas las llamadas , cambiar API_KEY Nueva: " )
+        print("[2] Exceeded calls, change API_KEY New: " )
         raw_response_2 = do_request_conunt_adder()
     return raw_response_2
 
-
-for S in ["ABNB"] + list_companys_FVOLA:
+#**DOCU**
+#1.0 Data collection
+# 1.0 (Recommended) The yfinance API, if you want price to price intervals in 15min intervals is limited to 2 months, to get more time data up to 2 years back (more data better predictive models) use the free version of the API https://www.alphavantage.co/documentation/
+#
+# Run Utils/API_alphavantage_get_old_history.py
+# The class is customizable: action intervals, months to ask and action ID.
+# Note: being the free version, there is a portrait between request and request, to get a single 2 years history it takes 2-3 minutes per action.
+# Once executed, the d_price/RAW_alpha folder will be filled with OHLCV historical .csv files of stock prices. These files will be read in the next step. Example name: alpha_GOOG_15min_20221031__20201112.csv
+print("Get the history of the stock 2 years back through the use of free alphavantage.co API keys")
+for S in list_companys:
     df_S_all = pd.DataFrame()
     for time_op in ALL_TIME_OPTIONS:
         raw_response = do_request()
@@ -97,9 +105,5 @@ for S in ["ABNB"] + list_companys_FVOLA:
     df_S_all['Date'] = df_S_all['Date'].astype(str)
     max_recent_date = df_S_all['Date'].max()[:10].replace('-','') # pd.to_datetime().strftime("%Y%m%d")
     min_recent_date = df_S_all['Date'].min()[:10].replace('-','') #pd.to_datetime( ).strftime("%Y%m%d")
-    print("../d_price/RAW/alpha_"+S+'_'+intelval+"_"+ max_recent_date + "__" + min_recent_date + ".csv")
+    print("../d_price/RAW_alpha/alpha_"+S+'_'+intelval+"_"+ max_recent_date + "__" + min_recent_date + ".csv")
     df_S_all.to_csv("../d_price/RAW_alpha/alpha_"+S+'_'+intelval+"_"+ max_recent_date + "__" + min_recent_date + ".csv", sep="\t", index=None)
-
-# df_S_raw = pd.DataFrame([x.split(',') for x in ],
-#                         columns=['Date', 'Open', 'Close', 'High', 'Low', 'Close_avg', 'Volume', "Adj"])
-
