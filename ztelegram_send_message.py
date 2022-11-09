9533ac0d4ee42ac72234439d65e396b2e030efef
Leci37 <leciluis@gmail.com>
2022-11-09 01:10:31 +0100

    refactor

--- ztelegram_send_message.py
+++ ztelegram_send_message.py
@@ -1,11 +1,3 @@
-# Done! Congratulations on your new bot. You will find it at t.me/Whale_Hunter_Alertbot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
-#
-# Use this token to access the HTTP API:
-# 5452553430:AAH8ARcZlQZFZHxckJuY0eWnUK08IKo6QnY
-# Keep your token secure and store it safely, it can be used by anyone to control your bot.
-#
-# For a description of the Bot API, see this page: https://core.telegram.org/bots/api
-
 #https://medium.com/codex/using-python-to-send-telegram-messages-in-3-simple-steps-419a8b5e5e2
 import requests
 import pandas as pd
@@ -13,27 +5,18 @@ from datetime import datetime
 
 from telegram.constants import ParseMode
 
+
 import yhoo_history_stock
 from LogRoot.Logging import Logger
-from Model_predictions_Nrows import get_RealTime_buy_seel_points
 from Utils import Utils_send_message
 from a_manage_stocks_dict import Option_Historical, DICT_COMPANYS , Op_buy_sell
-
-TOKEN = "5452553430:AAH8ARcZlQZFZHxckJuY0eWnUK08IKo6QnY"
-url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
-
-
-chat_idLUISL = "5058733760" # LUIS
-chat_idIVAN = "563544091"   #Ivan
-chat_idARIZT = "2073387965" # tania dosil
-chat_idTANIA= "495451714"   # tania dosil
-LIST_PEOPLE_IDS_CHAT = [chat_idLUISL, chat_idARIZT, chat_idTANIA ]
+from ztelegram_send_message_handle import URL_TELE, send_mesage_all_people, send_exception
 
 message = "hello from your telegram bot"
 
 
 
-feedback = requests.get(url).json()
+feedback = requests.get(URL_TELE).json()
 for dict_userId in  feedback['result']:
     print(dict_userId)
 
@@ -46,9 +29,6 @@ all_cols = ['Date' 'buy_sell_point' 'Close' 'has_preMarket' 'Volume' 'sum_r_88',
 prety_cols = ['Date' , 'sum_r_88', 'sum_r_93', 'have_to_oper', 'sum_r_TF', 'have_to_oper_TF', 'num_models']
 BOT = None
 
-
-CSV_NAME = "@VOLA"
-list_stocks = DICT_COMPANYS[CSV_NAME]
 
 NUM_MIN_MODLES  = 3
 NUM_MIN_MODLES_TF = 1
@@ -129,73 +109,36 @@ def send_alert(S, df_b_s, type_b_s):
         send_mesage_all_people(message_aler)
 
 
-def send_mesage_all_people(message_aler, parse_type = ParseMode.HTML):
-    botsUrl = f"https://api.telegram.org/bot{TOKEN}"  # + "/sendMessage?chat_id={}&text={}".format(chat_idLUISL, message_aler, parse_mode=ParseMode.HTML)
-    # url = botsUrl + "/sendMessage?chat_id={}&text={}&parse_mode={parse_mode}".format(chat_idLUISL, message_aler,parse_mode=ParseMode.MARKDOWN_V2)
-    print("Enviar alerta todo el mundo: " + message_aler)
-    for people_id in LIST_PEOPLE_IDS_CHAT:
-        url = botsUrl + "/sendMessage?chat_id={}&text={}&parse_mode={parse_mode}".format(people_id, message_aler,parse_mode=parse_type)
-        print(requests.get(url).json())
-
-def monitor_all_stocks_and_send_alerts():
-    print("START ciclo ")
-    print("START ciclo " + datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
-    print("START ciclo ")
-    for S in DICT_COMPANYS["@FOLO3"] : #list_stocks:  # list_stocks: # [ "UBER","U",  "TWLO", "TSLA", "SNOW", "SHOP", "PINS", "NIO", "MELI" ]:#list_stocks:
-        try:
-            print("Company: "+S)
-            df_compar, df_vender = get_RealTime_buy_seel_points(S, Option_Historical.DAY_6, NUM_LAST_REGISTERS_PER_STOCK=3)
-            # df_compar = pd.read_csv("Models/LiveTime_results/" + S + "_" + type_buy_sell.value + "_" + "_.csv",index_col=False, sep='\t')
-            if df_compar is not None:
-                send_alert(S, df_compar, Op_buy_sell.POS)
-
-            # df_vender = pd.read_csv("Models/LiveTime_results/" + S + "_" + type_buy_sell.value + "_" + "_.csv",index_col=False, sep='\t')
-            if df_vender is not None:
-                send_alert(S, df_vender, Op_buy_sell.NEG)
-        except Exception as ex:
-            send_exception(ex)
-
-    df_res = pd.DataFrame([[datetime.today().strftime('%Y-%m-%d %H:%M:%S'), '----', '----','----','----', '----%', '----%', '----%', '----%', datetime.today().strftime('%Y-%m-%d %H:%M:%S')]], columns=['Date', 'Stock', 'buy_sell','Close', '88%', '93%', '95%', 'TF%', "Models_names"])
-    df_res.to_csv( "zTelegram_Registers.csv", sep="\t", index=None, mode='a', header=False)
-    print("END ciclo ")
-    print("END ciclo "+ datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
-    print("END ciclo ")
 
 
-def send_exception(ex, extra_mes = ""):
-    message_aler = extra_mes + "   ** Exception **" + str(ex)
-    print(message_aler)
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_idLUISL}&text={message_aler}"
-    print("Enviar alerta: " + message_aler)
-    print(requests.get(url).json())
-
-
-list_most_probable = ["UPST", "MELI", "TWLO", "RIVN", "SNOW", "LYFT", "UBER","QCOM", "PYPL",  "RUN", "GTLB", #=['EPAM'] #
- "MDB", "NVDA",  "ADSK", "BABA",    "ABNB", "TSLA", "META",
- "PTON",  "NVST", "HUBS", "EPAM", "PINS", "TTD", "SNAP", "APPS", "AFRM", "DOCN",
- "SHOP", "NIO", "U", "GME", "RBLX", "CRSR"]
-
-
-#https://github.com/python-telegram-bot/python-telegram-bot/wiki/Introduction-to-the-API
-
-# async def main():
-#     BOT = telegram.Bot(TOKEN)
-#     async with BOT:
-#         print(await BOT.get_me())
+# def monitor_all_stocks_and_send_alerts():
+#     print("START ciclo ")
+#     print("START ciclo " + datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+#     print("START ciclo ")
+#     for S in DICT_COMPANYS["@FOLO3"] : #list_stocks:  # list_stocks: # [ "UBER","U",  "TWLO", "TSLA", "SNOW", "SHOP", "PINS", "NIO", "MELI" ]:#list_stocks:
+#         try:
+#             print("Company: "+S)
+#             df_compar, df_vender = Model_predictions_handle_Nrows.get_RealTime_buy_seel_points(S, Option_Historical.DAY_6, NUM_LAST_REGISTERS_PER_STOCK=3)
+#             # df_compar = pd.read_csv("Models/LiveTime_results/" + S + "_" + type_buy_sell.value + "_" + "_.csv",index_col=False, sep='\t')
+#             if df_compar is not None:
+#                 send_alert(S, df_compar, Op_buy_sell.POS)
+#
+#             # df_vender = pd.read_csv("Models/LiveTime_results/" + S + "_" + type_buy_sell.value + "_" + "_.csv",index_col=False, sep='\t')
+#             if df_vender is not None:
+#                 send_alert(S, df_vender, Op_buy_sell.NEG)
+#         except Exception as ex:
+#             send_exception(ex)
+#
+#     df_res = pd.DataFrame([[datetime.today().strftime('%Y-%m-%d %H:%M:%S'), '----', '----','----','----', '----%', '----%', '----%', '----%', datetime.today().strftime('%Y-%m-%d %H:%M:%S')]], columns=['Date', 'Stock', 'buy_sell','Close', '88%', '93%', '95%', 'TF%', "Models_names"])
+#     df_res.to_csv( "zTelegram_Registers.csv", sep="\t", index=None, mode='a', header=False)
+#     print("END ciclo ")
+#     print("END ciclo "+ datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+#     print("END ciclo ")
 
 
 
-# if __name__ == '__main__':
 
-print("START ciclo ")
-print("START ciclo "+ datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
-print("START ciclo ")
-
-# starttime = time.time()
-# while True:
-#     print("tick")
-
-from datetime import datetime
+# from datetime import datetime
 #Para ejecutar algo cada 15 minutos después de la hora.
 # while 1:
 #     dt = datetime.now() + timedelta(minutes=15)
