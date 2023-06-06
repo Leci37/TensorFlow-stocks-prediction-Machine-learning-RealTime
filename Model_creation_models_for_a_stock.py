279548a2f81a49f8896100055775c9b9e5efb9c6
Vimal Patnaik <vimal.patnaik@dhamma.org>
2023-06-06 12:59:31 +0530

    Added USE_GPU as a parameter in the config file

--- Model_creation_models_for_a_stock.py
+++ Model_creation_models_for_a_stock.py
@@ -1,14 +1,24 @@
 """https://github.com/Leci37/LecTrade LecTrade is a tool created by github user @Leci37. instagram @luis__leci Shared on 2022/11/12 .   . No warranty, rights reserved """
 import pandas as pd
 import tensorflow as tf
+import _KEYS_DICT
 from tensorflow import keras
 from keras.backend import set_session
-# Allow memory growth for the GPU
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
-# Assume that you have 12GB of GPU memory and want to allocate ~4GB:
-gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.333)
-sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options))
+
+# Use GPU or CPU
+if _KEYS_DICT.USE_GPU.lower() == 'yes' or _KEYS_DICT.USE_GPU.lower() == 'y':
+    # Allow memory growth for the GPU
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    # Assume that you have 12GB of GPU memory and want to allocate ~4GB:
+    gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=_KEYS_DICT.PER_PROCESS_GPU_MEMORY_FRACTION)
+    sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options))
+elif _KEYS_DICT.USE_GPU.lower() == 'no' or _KEYS_DICT.USE_GPU.lower() == 'n':
+    physical_devices = tf.config.experimental.list_physical_devices('CPU')
+else:
+    print("Invalid response for USE_GPU in _KEYS_DICT.py file. Going to assume CPU ")
+    physical_devices = tf.config.experimental.list_physical_devices('CPU')
+
 
 # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
 # sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
