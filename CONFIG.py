b023ccfc05e98d59e1df94475b855b2c0f49a9d1
Luis Le <110530848+Leci37@users.noreply.github.com>
2023-07-17 16:56:39 +0200

    upload tutorial
    Signed-off-by: Luis Le <110530848+Leci37@users.noreply.github.com>

---
+++ CONFIG.py
@@ -0,0 +1,96 @@
+
+
+TIME_WINDOW_GT  = 36 #Roling windows for GT
+INDI_TIMEFRAME = ['5min', '15min', '60min']
+
+DICT_COMPANYS = {
+    "@FAV":
+        ['U' ] # ['U' , 'UPST']
+}
+
+
+INDicator_label_3W51560_ = "_3W51560_"
+INDicator_label_3W5_ = "_3W5_"
+INDicator_label_3W515_ = "_3W515_"
+INDicator_label_3W560_ = "_3W560_"
+INDI_TIMEFRAME_OPTIONS = {
+    INDicator_label_3W51560_:['5min', '15min', '60min'],
+    INDicator_label_3W5_ :   ['5min'],
+    INDicator_label_3W515_ : ['5min', '15min'],
+    INDicator_label_3W560_ : ['5min', '60min']
+}
+
+
+
+# Training Settings
+SPLIT_RATIO_X_Y = 0.80
+val_ratio=0.2
+WINDOWS_SIZE = 48 # 48 cuatro horas
+predict_period = 1
+
+indicator_timeframe = "H1"
+timeframe = 'M15'
+
+# Symbol Information
+symbols = {
+    'EURUSD': { 'point': 10000, 'spread': 0.4 },
+    'GBPUSD': { 'point': 10000, 'spread': 0.6 },
+    'USDCAD': { 'point': 10000, 'spread': 1.4 },
+    'USDCHF': { 'point': 10000, 'spread': 1.4 },
+    'USDJPY': { 'point': 100,   'spread': 1.4 },
+    'GBPJPY': { 'point': 100,   'spread': 2.6 },
+    'AUDUSD': { 'point': 10000, 'spread': 1.4 },
+    'XAUUSD': { 'point': 10,    'spread': 17.0},
+}
+
+use_percentage = False
+NUMS_FEATURES = 45
+
+# Multi Environment settings
+train_limit = 53000
+
+# Max processes for tech df parallel processing
+MAX_PROCESSES = 10
+
+
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+#should be removed from the training because they use global averages, i.e. if you remove the last column, they change the value of all previous columns, useless in realtime.
+LIST_TECH_REMOVE_NOT_EQUAL_IF_REMOVE_THE_FIRSH = ["mtum_APO", "mtum_CCI", "mtum_MACD_ext", "mtum_MACD_ext_signal", "mtum_MACD_ext_list", "mtum_PPO",
+                                                  "mtum_STOCH_RSI_d", "mtum_STOCH_RSI_kd", "ma_TRIMA_5", "ma_TRIMA_20", "ma_WMA_20", "ma_TRIMA_50",
+                                                  "ma_TRIMA_100", "mtum_BIAS_SMA_26", "mtum_BR_26", "olap_VMAP", "perf_CUMLOGRET_1", "perf_CUMPCTRET_1", "sti_ENTP_10"]
+#should be removed because they generate a lot of None, in the last columns, useless in RealTime.
+LIST_TECH_REMOVE_GENERATED_NONE_LAST = ['ti_acc_dist']
+LIST_TECH_REMOVE_GENERATED_INFINITE = ['olap_MCGD_10']
+
+LIST_TECH_REMOVE = LIST_TECH_REMOVE_NOT_EQUAL_IF_REMOVE_THE_FIRSH + LIST_TECH_REMOVE_GENERATED_NONE_LAST +LIST_TECH_REMOVE_GENERATED_INFINITE
+
+dict_correlations = {
+    "v1" : ['chi2'],
+    "v2" : ['ExtraTrees'],
+    "v3" : ['corrwith'],
+    "v4" : ['f_regression'],
+    "v5" : ['chi2','ExtraTrees'],
+    "v6" : ['chi2','corrwith'],
+    "v7" : ['chi2','f_regression'],
+    "v8" : ['ExtraTrees','corrwith'],
+    "v9" : ['ExtraTrees','f_regression'],
+    "v10" : ['corrwith','f_regression'],
+    "v11" : ['chi2','ExtraTrees','corrwith'],
+    "v12" : ['chi2','ExtraTrees','f_regression'],
+    "v13" : ['chi2','corrwith','f_regression'],
+    "v14" : ['ExtraTrees','corrwith','f_regression'],
+    "v15" : ['chi2','ExtraTrees','corrwith','f_regression']
+}
--- buy_sell_Train_3W_5min_RT.py
+++ buy_sell_Train_3W_5min_RT.py
@@ -5,6 +5,9 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from sklearn.utils.class_weight import compute_class_weight
+from keras.optimizers import Adam
+
+from features_W3.ta import extract_features_old
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
@@ -13,8 +16,8 @@ physical_devices = tf.config.experimental.list_physical_devices('CPU')
 
 from utilW3 import buy_sell_point, Feature_selection_create_json, get_info_model_evaluation
 from utilW3.data import normalise_data, get_window_data, split_data, set_save_class_weight, get_load_class_weight
-import features
-from features.extract import uncorrelate_selection , get_tree_correlation
+import features_W3
+from features_W3.extract import uncorrelate_selection , get_tree_correlation
 from CONFIG import *
 
 # Disable Tensorflow useless warnings
@@ -32,7 +35,7 @@ INDicator_label_3W5_ = "_3W5_"
 
 # list_options_w =  [INDicator_label_3W5_, INDicator_label_3W51560_ ,INDicator_label_3W515_, INDicator_label_3W560_ ]
 # stocks_list = [  "U", "DDOG","UPST", "RIVN", "SNOW", "LYFT",  "GTLB"] + [ "MDB", "HUBS", "TTD", "ASAN",    "APPS" , "DOCN", "SHOP", "RBLX", "NIO"]
-stocks_list = [ "RIVN"]
+stocks_list =  [   "RIVN"]#,"UPST", "RIVN", "SNOW", "LYFT",  "GTLB"] + [ "MDB", "HUBS", "TTD", "ASAN",    "APPS" , "DOCN", "SHOP", "RBLX", "NIO"]
 
 
 INDI = INDicator_label_3W5_
@@ -43,41 +46,30 @@ for symbol in stocks_list : #stocks_list:
 
     features_X_col = []
     ind  = "5min"
-
-    # Read ohlcv data
+    # [1]  Read ohlcv data
     file_rwa_alpha =  f'../d_price/tutorial_{symbol}_{ind}' + ".csv"
     #TIP to get more olhcv data can use 0_API_alphavantage_get_old_history.py (in this project) or alpaca API
-    file_rwa_alpha = "../d_price/RAW_alpha/GTLB_5min_20230616__20211014.csv"
     df_raw = pd.read_csv(file_rwa_alpha, parse_dates=['Date'], index_col='Date', sep='\t')
     for c in df_raw.columns:
         df_raw = df_raw.rename(columns={c: c.lower()})
+    df_raw.index = pd.to_datetime(df_raw.index, errors="coerce")
     print("\n[1] Read data ohlcv data  path ", file_rwa_alpha, ' Shape_raw: ', df_raw.shape)
     #TIP tip avoid using data older than 2 years, old data not useful data
 
-    # Get the 830 tech paterns
+    # [2] Get the 292 tech paterns
     print("\n[2] Calculating Technical indicators. stock: ", symbol )
-    # df_S = features.ta.extract_features(df_raw)#TODO optimise, calculate the 830, It just need the +-90 from the .json
-    from features import extract_features_v3
-    df_S_2 = extract_features_v3(df_raw)
-    # df_S_2['date'] = df_S_2.index
-    # print('Generated: ',  f'data/GTLB_test_generate.csv')
-    # df_S_2.to_csv(f'data/GTLB_test_generate.csv',sep="\t")
-
-    # df_features_X_ALL = features.ta.extract_features(df_raw)
+    # df_bars = features_W3.ta.extract_features(df_raw)#TODO optimise, calculate the 830, It just need the +-90 from the .json
+    #TODO review why using the 800 indicators reduces the effectiveness of the 270 indicators.
+    from features_W3_old import extract_features_v3
+    df_bars = extract_features_v3(df_raw)  # IT WORKS   Tech indicators Count:  292
+    # from features_W3_old.ta import extract_features
+    # df_bars = extract_features(df_raw)# IT does not WORKS   Tech indicators Count:  800
     # TIP The technical indicators have been cleaned (LIST_TECH_REMOVE) , but it is possible that some of them may be reoccasing data from the future, in order to make the calculation, these must be discarded for a correct functioning in real time.
-    # df_S_2 = pd.read_csv(r'E:\TradeDL\data\U_5min_TEST_Z_tech_all.csv', parse_dates=['date'], index_col='date', sep='\t')
-    # df_S_2 = pd.read_csv(r'E:\TradeDL\data\U_5min_TEST_tech_all.csv', parse_dates=['date'], index_col='date', sep='\t')
-    # df_S_2.index = pd.to_datetime(df_S_2.index, errors="coerce")
-    df_S_2 = df_S_2.drop(columns=LIST_TECH_REMOVE + ['Date', 'date', 'Date.1', 'date.1'], errors='ignore')
-
-    print("[2] Calculated Technical indicators. stock: ",symbol," Tech indicator count: ",  df_S_2.shape )
-
-    df_S_2 = df_S_2.drop(columns=LIST_TECH_REMOVE+ ['Date','date'], errors='ignore') #TODO no genere, it is bad tech paterns
-    df_bars = df_S_2
-    # df_S_3 = df_S_3.drop(columns=LIST_TECH_REMOVE + ['Date', 'date'], errors='ignore')
+    print("[2.1] Calculated Technical indicators. stock: ",symbol," Tech indicator count: ",  df_bars.shape )
+    df_bars = df_bars.drop(columns=LIST_TECH_REMOVE+ ['Date','date', 'Date.1','date.1'], errors='ignore') #TODO no generate LIST_TECH_REMOVE, it is bad tech paterns
     # see the diferenet bettewn two list set(df_bars.columns) ^ set(df_S_2.columns)
-    # set(df_S_2.columns) ^ set(df_S_3.columns)
-    # Get the target_Y (ground true)
+
+    # [3] Get the target_Y (ground true)
     print("\n[3] Calculate the target_Y (ground true) ,what is the target to detect?  the highest and lowest peaks stock: ", symbol)
     target_Y , df_l, _= buy_sell_point.get_buy_sell_point_HT_pp(df_bars.copy(), period=TIME_WINDOW_GT, rolling=TIME_WINDOW_GT * 2)
     # TIP the GT detection should be improved, it only detects the maximum and minimum points in time window,
@@ -86,7 +78,7 @@ for symbol in stocks_list : #stocks_list:
     features_X_ALL = df_bars
     print("[3.2] DEBUG All technical indicators Extracted Count: ",len(features_X_ALL.columns)) #," Names : ",",".join( features_X_ALL.columns ))
 
-    #Do selection best tech params
+    # [4] Do selection best tech params
     print("\n[4] Calculation of correlation strength. What are the +-100 best technical indicators and which are noise ")
     print("[4.1]uncorrelate_selection path: ", f'data/columns_select/{symbol}_{REF_MODEL}_columns.pkl')
     features_X_col = uncorrelate_selection(features_X_ALL, num_features=NUMS_FEATURES, split_ratio=SPLIT_RATIO_X_Y, column_path=f'data/columns_select/{symbol}_{REF_MODEL}.pkl')
@@ -95,18 +87,18 @@ for symbol in stocks_list : #stocks_list:
     columns_json_BOTH = list(set(list(features_X_col.columns.values) + list(features_X_col_tree)  ))
     Feature_selection_create_json.created_json_feature_selection(list_all_columns=columns_json_BOTH,path_json=f'data/columns_select/{symbol}_{REF_MODEL}_corr.json')
     features_X = features_X_ALL[columns_json_BOTH]
-    print("[4.2] Select the best technical patterns to train with features Extracted Count: ", len(features_X.columns), " Names : ",",".join(features_X.columns))
-    print("[4.3] features index Dates: from ", features_X.index[0]," to ", features_X.index[-1], " Shape: ", features_X.shape)
+    print("[4.2] Select the best technical patterns to train with features_W3 Extracted Count: ", len(features_X.columns), " Names : ",",".join(features_X.columns))
+    print("[4.3] features_W3 index Dates: from ", features_X.index[0]," to ", features_X.index[-1], " Shape: ", features_X.shape)
     target_Y = target_Y[target_Y.index.isin(features_X.index)]
     features_X = features_X[features_X.index.isin(target_Y.index)]
 
-    #Normalizated
-    print("\n[5] For correct training, values between 0 and 1 are required  ")
-    #TIP   the data is normalised is stored in 'data/scalers/{symbol}_{REF_MODEL}_scalex.pkl'   for use it in real time with only the last ohlcv data.
+    # [5] Normalizated
+    print("\n[5] For correct training, for correct training the values must be normalised to between 0 and 1  ")
+    #TIP   how to normalise this data is stored in file 'data/scalers/{symbol}_{REF_MODEL}_scalex.pkl' for use in the future realtime  .
     print("[5.1] Normalise_data path: ", f'data/scalers/{symbol}_{REF_MODEL}_scalex.pkl')
     features_X, scaleX = normalise_data(features_X, SPLIT_RATIO_X_Y, f'data/scalers/{symbol}_{REF_MODEL}_scalex.pkl')
 
-    # Get Window Data
+    # [6] Get Window Data
     print("\n[6] Currently you have for each target_Y value a row of technical indicators, you add a 'window'"
           " to make the decision to predict whether the +-48 rows above will be taken (about 4 hours of previous indicators, splited in 5min).")
     X, y, index = get_window_data(features_X, target_Y, window_size=WINDOWS_SIZE)
@@ -114,13 +106,13 @@ for symbol in stocks_list : #stocks_list:
     # Example 'INFO created array x_array Created x_array.shape : (54000, 3, 48, 101) ,EX: 54000 rows, 3 clandle times (5min,15mim.60min) , windows size 48, 101tech parents' Maybe add a one minute candle as well. method get_window_data_3Window()
     print("[6.1] get_window_data Shapes Y:" , y.shape, " X: ", X.shape)
 
-    # SPLIT the Data
+    # [7] SPLIT the Data
     print("\n[7] data split between training and validation ")
     X_train, y_train, X_test, y_test, index_train, index_test = split_data(X, y, index, split_ratio=SPLIT_RATIO_X_Y)
     print("[7.1] Shapes: X_train: ",X_train.shape, " y_train:  ", y_train.shape, " index_train:  ", index_train.shape)
     # TIP  the tests I did by shuffling the data , destroy the result , I don't understand why there are time windows to save the relationship.
 
-    # Get  class_weight
+    # [8] Get  class_weight
     print("\n[8] Ground True data are unbalanced  Given that there is a lot of 'do nothing' 0, and very little 'do buy' 1 or 'do sell' 2,"
           " weight balancing is required, to give more importance to the minorities.  ")
     class_weight = compute_class_weight('balanced', classes=np.unique(np.argmax(y_train, axis=1)), y=np.argmax(y_train, axis=1))
@@ -130,12 +122,13 @@ for symbol in stocks_list : #stocks_list:
     # TIP tip the balancing to check the unbalance is very necessary, I would also like to add that it penalizes more in the training the false positives,
     # there are other methods for balancing as SMOTE, they have been tested without improvement, in my opinion, I don't like them, they destroy the data, if it is applied it must be very delicate.
 
-    # Create a model TF
+    # [9] Create a model TF
     print("\n[9] Creation of the TF model architecture. must respect the input_shape and output_shape and the 'softmax' ,"
           " from there EXPRIMENT combinations ")
     input_shape = X_train.shape[1:]
     output_shape = 1 if len(y_train.shape) == 1 else y_train.shape[1]
     print("[9.1] arrays to use the TF model  , input_shape: ", input_shape, " output_shape: ",output_shape)
+    # TIP to realy undertand this point visit and play with neuron web https://playground.tensorflow.org/#activation=relu&batchSize=15&dataset=spiral&regDataset=reg-plane&learningRate=0.001&regularizationRate=0&noise=50&networkShape=8,8,4,2&seed=0.94804&showTestData=false&discretize=false&percTrainData=50&x=false&y=false&xTimesY=true&xSquared=false&ySquared=false&cosX=false&sinX=true&cosY=false&sinY=true&collectStats=false&problem=classification&initZero=false&hideText=false
     #TODO TIP , train multiples models types and get the best  , The creation of the TF architecture can be done in a thousand ways, here is just one example, try the different ways:
         # Models to test on https://github.com/Leci37/stocks-prediction-Machine-learning-RealTime-telegram#combine-the-power-of-the-17-models
         # Review of all forms of time series prediction: lstm,gru,cnn and rnn https://github.com/Leci37/stocks-prediction-Machine-learning-RealTime-telegram#review-of-all-forms-of-time-series-prediction-lstmgrucnn-and-rnn
@@ -155,11 +148,12 @@ for symbol in stocks_list : #stocks_list:
         keras.layers.Dense(output_shape, activation='softmax')#, bias_initializer = output_bias_k),
     ])
     #TIP must respect the input_shape and output_shape and the 'softmax' , he rest can be edited for EXPRIMENT combinations
-    model.compile(loss=tf.keras.losses.CategoricalCrossentropy(), optimizer='adam', metrics=tf.keras.metrics.CategoricalAccuracy())
+    model.compile(loss=tf.keras.losses.CategoricalCrossentropy(), optimizer=Adam(lr=0.001), metrics=tf.keras.metrics.CategoricalAccuracy())
+    print("[9.2] optimicer config : ", model.optimizer.get_config())
     tf.keras.utils.plot_model(model, to_file=f'outputs/plots/{symbol}_{REF_MODEL}_buysell.png')
-    print("[9.2] print diagram of the TF model Path: ", f'outputs/plots/{symbol}_{REF_MODEL}_buysell.png')
+    print("[9.3] print diagram of the TF model Path: ", f'outputs/plots/{symbol}_{REF_MODEL}_buysell.png')
 
-
+    # [10] Trian the model
     print("\n[10] Start training "+ bcolors.OKBLUE  , f'outputs/{symbol}_{REF_MODEL}_buysell.h5' , bcolors.ENDC)
     # with tf.device('/cpu:0'): # to improve the eficent https://stackoverflow.com/questions/62916904/failed-copying-input-tensor-from-cpu-to-gpu-in-order-to-run-gatherve-dst-tensor
     #     X_train = tf.convert_to_tensor(X_train, np.float32); y_train = tf.convert_to_tensor(y_train, np.float32)
@@ -173,8 +167,10 @@ for symbol in stocks_list : #stocks_list:
     print("[10.1] Model initial_weights saved : ",  f'data/initial_weights/{symbol}_{REF_MODEL}_initial_weights.tf')
     model.save_weights( f'data/initial_weights/{symbol}_{REF_MODEL}_initial_weights.tf')
 
+    # [11] predict and eval
     print('\n[11] Do a predict and eval. Load path: ', f'outputs/{symbol}_{REF_MODEL}_buysell.h5')
     print("[11.1] array to predit x_test.shape: ", X_test.shape)
+    gc.collect()  # IMPORTANT clean the ram
     # load_class_weight = get_load_class_weight(f'data/class_weight/{symbol}_{REF_MODEL}_class_weight.pkl')
     resampled_model_2 = keras.models.load_model(f'outputs/{symbol}_{REF_MODEL}_buysell.h5')
     y_pred_1 = resampled_model_2.predict(X_test, batch_size=6)
@@ -188,7 +184,7 @@ for symbol in stocks_list : #stocks_list:
     # TIP check the y_pred y_test table , to know what is the proportion of failures and successes in accurate prediction.
     # TODO improve the evaluation system of the model, not answering the question "how many times did I get it right", but the question "how much money did I win or lose?
     # TODO for more ideas on possible improvements, check out the section  https://github.com/Leci37/stocks-prediction-Machine-learning-RealTime-telegram#possible-improvements
-    gc.collect()#IMPORTANT clean the ram
+    gc.collect()
     print("\n")
 print("END")
 
