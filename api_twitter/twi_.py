ad99fe603df129b60a9dc9ad3be0f7c1f89e4388
Luis Le <110530848+Leci37@users.noreply.github.com>
2023-10-18 11:57:53 +0200

    update api twitter ways
    #https://stackoverflow.com/questions/76810100/how-do-i-fix-403-forbidden-error-when-using-tweepy-to-access-the-twitter-api/76810679#76810679

--- api_twitter/twi_.py
+++ api_twitter/twi_.py
@@ -48,30 +48,57 @@ from api_twitter import twi_credential
 # except:
 #     print("Error during authentication")
 
-import tweepy
-api = None
-
-def twitter_api():
-    global api
-    if api is None:
-        consumer_key = twi_credential.API_Key # os.getenv('TWITTER_CONSUMER_KEY')
-        consumer_secret = twi_credential.API_Key_Secret # os.getenv('TWITTER_CONSUMER_SECRET')
-        access_token = twi_credential.Access_Token #os.getenv('TWITTER_ACCESS_TOKEN')
-        access_token_secret = twi_credential.Access_Token_Secret #os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
-
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)
-
-        api = tweepy.API(auth)
+# def twitter_api_v1():
+#     global TW_API_CREDENT
+#     if TW_API_CREDENT is None:
+#         consumer_key = twi_credential.API_Key # os.getenv('TWITTER_CONSUMER_KEY')
+#         consumer_secret = twi_credential.API_Key_Secret # os.getenv('TWITTER_CONSUMER_SECRET')
+#         access_token = twi_credential.Access_Token #os.getenv('TWITTER_ACCESS_TOKEN')
+#         access_token_secret = twi_credential.Access_Token_Secret #os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+#
+#         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+#         auth.set_access_token(access_token, access_token_secret)
+#
+#         TW_API_CREDENT = tweepy.API(auth)
+#         Logger.logr.info("\nLOAD twitter_api \tAPI.user_agent: "+ str( TW_API_CREDENT.user_agent) )
     #     return api_tw
     # else:
     #     return api_tw
 
+
+import tweepy
+TW_API_CREDENT = None
+API_client = None
+API_auth = None
+#https://stackoverflow.com/questions/76810100/how-do-i-fix-403-forbidden-error-when-using-tweepy-to-access-the-twitter-api/76810679#76810679
+def twitter_api_v2():
+    global API_client, API_auth, TW_API_CREDENT
+    if TW_API_CREDENT is None or API_client is None:
+        API_client = tweepy.Client(
+            consumer_key=twi_credential.API_Key,
+            consumer_secret=twi_credential.API_Key_Secret,
+            access_token=twi_credential.Access_Token,
+            access_token_secret=twi_credential.Access_Token_Secret,
+        )
+        API_auth = tweepy.OAuth1UserHandler(
+            twi_credential.API_Key,
+            twi_credential.API_Key_Secret,
+            twi_credential.Access_Token,
+            twi_credential.Access_Token_Secret
+        )
+
+        # Create API object
+        TW_API_CREDENT = tweepy.API(API_auth, wait_on_rate_limit=True)
+        Logger.logr.info("\nLOAD twitter_api \tAPI.user_agent: " + str(TW_API_CREDENT.user_agent))
+
+
+
 #Put your Bearer Token in the parenthesis below
-client = tweepy.Client(bearer_token=twi_credential.Bearer_Token)
+# client = tweepy.Client(bearer_token=twi_credential.Bearer_Token)
 def create_simple_tweet(text):
-    twitter_api()
-    tweet = api.update_status(status = text)
+    global TW_API_CREDENT
+    twitter_api_v2()
+    tweet = TW_API_CREDENT.update_status(status = text)
     return tweet
 
 
@@ -79,21 +106,24 @@ def create_simple_tweet(text):
 # path_tech = r"C:\Users\Luis\Desktop\LecTrade\LecTrade\plots_relations\Trader_View_png\AMD_TRAVIEW_tech.png"
 # path_finan =  r"C:\Users\Luis\Desktop\LecTrade\LecTrade\plots_relations\Trader_View_png\AMD_TRAVIEW_finan.png"
 def put_tweet_with_images(text, list_images_path:list, in_reply_to_status_id = None):
-    twitter_api()
-    Logger.logr.debug("Tweet will Send  " + str(text) )
-
+    global TW_API_CREDENT
+    twitter_api_v2()
+    # Logger.logr.debug("Tweet will Send  " + str(text) )
 
     # try:
     list_media = []
     for i in list_images_path:
-        list_media.append(api.media_upload(filename = i).media_id_string)
-    print("MEDIA: images found :", len(list_media), " Media_id Strigs: ", ", ".join(list_media))
+        list_media.append(TW_API_CREDENT.media_upload(filename=i).media_id)
+    print("MEDIA: images found :", len(list_media), " Media_id Strigs: ", ', '.join(str(v) for v in list_media))
 
-    if in_reply_to_status_id is None:
-        tweet_sent = api.update_status( status=str(text) , media_ids=list_media )
-    else:
-        tweet_sent = api.update_status(status=str(text) , media_ids=list_media, in_reply_to_status_id = in_reply_to_status_id)
-    Logger.logr.info("Tweet has sent  tweet_id_str: " + tweet_sent.id_str)
+    # Post tweet with image
+    tweet_sent = API_client.create_tweet(text=str(text).encode('utf-8', errors='replace').decode('utf-8'), media_ids=list_media)
+    Logger.logr.info("Tweet has sent  tweet_ID_str: " + tweet_sent.data['id'] ) #+ " Text:" + tweet_sent.data['text'])
+    # if in_reply_to_status_id is None: # .encode('utf-8', errors='replace').decode('utf-8')
+    #     tweet_sent = TW_API_CREDENT.update_status(status=str(text).encode('utf-8', errors='replace').decode('utf-8'), media_ids=list_media)
+    # else:
+    #     tweet_sent = TW_API_CREDENT.update_status(status=str(text).encode('utf-8', errors='replace').decode('utf-8'), media_ids=list_media, in_reply_to_status_id = in_reply_to_status_id)
+
     return tweet_sent
     # except Exception as ex:
     #     print("Error during authentication ", ex)
