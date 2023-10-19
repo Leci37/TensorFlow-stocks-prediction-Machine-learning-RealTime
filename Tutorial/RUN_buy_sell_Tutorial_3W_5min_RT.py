7f21f4dcca647972a8252ca4cde8f6ebece909e9
Luis Le <110530848+Leci37@users.noreply.github.com>
2023-10-19 23:20:47 +0200

    Add files via upload
    Signed-off-by: Luis Le <110530848+Leci37@users.noreply.github.com>

--- Tutorial/RUN_buy_sell_Tutorial_3W_5min_RT.py
+++ Tutorial/RUN_buy_sell_Tutorial_3W_5min_RT.py
@@ -15,12 +15,14 @@ print(os.getenv('TF_GPU_ALLOCATOR'))
 physical_devices = tf.config.experimental.list_physical_devices('CPU')
 
 from utilW3 import buy_sell_point, Feature_selection_create_json, get_info_model_evaluation
-from utilW3.data import normalise_data, get_window_data, split_data, set_save_class_weight, get_load_class_weight
+from utilW3.data import normalise_data, get_window_data, split_data, set_save_class_weight, get_load_class_weight, \
+    remove_strong_correlations_columns, get_best_Y_correlations_columns
 from features_W3_old.extract import uncorrelate_selection , get_tree_correlation
 from CONFIG import *
 
 # Disable Tensorflow useless warnings
 from tensorflow import keras
+# from tensorflow_addons.metrics import F1Score
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 tf.compat.v1.enable_eager_execution()
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
@@ -36,11 +38,21 @@ INDicator_label_3W5_ = "_3W5_"
 # stocks_list = [  "U", "DDOG","UPST", "RIVN", "SNOW", "LYFT",  "GTLB"] + [ "MDB", "HUBS", "TTD", "ASAN",    "APPS" , "DOCN", "SHOP", "RBLX", "NIO"]
 stocks_list =  ["RIVN"]
 
+def get_metrics(num_classes=3):
+    return [
+        keras.metrics.BinaryAccuracy(name='accuracy'),
+        keras.metrics.Precision(name='precision'),
+        keras.metrics.Recall(name='recall'),
+        # F1Score(name='f1_macro', num_classes=num_classes, average='macro'),
+        keras.metrics.AUC(name='auc'),
+        keras.metrics.AUC(name='prc', curve='PR'),  # precision-recall curve
+    ]
+
 
 INDI = INDicator_label_3W5_
 indicator_timeframe = INDI
 # for INDI in list_options_w:
-REF_MODEL = INDI+"_test_A1_"
+REF_MODEL = INDI+"_tut_A1_"
 for symbol in stocks_list : #stocks_list:
 
     features_X_col = []
@@ -60,7 +72,7 @@ for symbol in stocks_list : #stocks_list:
     # df_bars = features_W3.ta.extract_features(df_raw)#TODO optimise, calculate the 830, It just need the +-90 from the .json
     #TODO review why using the 800 indicators reduces the effectiveness of the 270 indicators.
     from features_W3_old import extract_features_v3
-    df_bars = extract_features_v3(df_raw)  # IT WORKS   Tech indicators Count:  292
+    df_bars = extract_features_v3(df_raw, extra_columns=True)  # IT WORKS   Tech indicators Count:  292
     # from features_W3_old.ta import extract_features
     # df_bars = extract_features(df_raw)# IT does NOT WORKS   Tech indicators Count:  800
     # TIP The technical indicators have been cleaned (LIST_TECH_REMOVE) , but it is possible that some of them may be GET data from the future, in order to make the calculation, these must be discarded for a correct functioning in real time.
@@ -72,20 +84,27 @@ for symbol in stocks_list : #stocks_list:
 
     # [3] Get the target_Y (ground true)
     print("\n[3] Calculate the target_Y (ground true) ,what is the target to detect?  the highest and lowest peaks stock: ", symbol)
+    #TODO How do you know that the clustering GroundTrue (buy, sell, nothing) is similar and correct?
+    # Is it necessary to cluster with more labels (buy_A, sell_A , nothing_A, buy_B, sell_B , nothing_B, buy_C, sell_C)? How do I know the number of tags needed ?
+    # To solve this question you can use the "K-means Clustering" https://neptune.ai/blog/clustering-algorithms
     target_Y , df_l, _= buy_sell_point.get_buy_sell_point_HT_pp(df_bars.copy(), period=TIME_WINDOW_GT, rolling=TIME_WINDOW_GT * 2)
     # TIP the GT detection should be improved, it only detects the maximum and minimum points in time window,
     # in my opinion THE MOST IMPORTANT it should detect when the candles are "sharpened", to detect the entry of the "sharpening".   more info: https://github.com/Leci37/stocks-prediction-Machine-learning-RealTime-telegram#11-data-collection
     print("[3.1]target_Y.shape: " ,target_Y.shape)
     features_X_ALL = df_bars
+
+   # Create correlation matrix https://stackoverflow.com/questions/29294983/how-to-calculate-correlation-between-all-columns-and-remove-highly-correlated-on
     print("[3.2] DEBUG All technical indicators Extracted Count: ",len(features_X_ALL.columns)) #," Names : ",",".join( features_X_ALL.columns ))
 
     # [4] Do selection best tech params
-    print("\n[4] Calculation of correlation strength. What are the +-100 best technical indicators and which are noise ")
+    print("\n[4] Calculation of correlation strength. What are the +-100 best technical indicators and which are noise. Remove strong correlacted columns")
     print("[4.1]uncorrelate_selection path: ", f'data/columns_select/{symbol}_{REF_MODEL}_columns.pkl')
-    features_X_col = uncorrelate_selection(features_X_ALL, num_features=NUMS_FEATURES, split_ratio=SPLIT_RATIO_X_Y, column_path=f'data/columns_select/{symbol}_{REF_MODEL}.pkl')
-    features_X_col_tree = get_tree_correlation(features_X_ALL, target_Y, num_features=20)
+    features_X_ALL = remove_strong_correlations_columns(features_X_ALL,factor=0.85)
+    features_X_col_corr = get_best_Y_correlations_columns(features_X_ALL.copy(), target_Y, num_columns=NUMS_FEATURES+20)
+    features_X_col_tree = get_tree_correlation(features_X_ALL.copy(), target_Y, num_features=22)
     # TIP  all these convolutions have been tried to find out which has the strongest relationship in Tutorial/CONFIG.dict_correlations
-    columns_json_BOTH = list(set(list(features_X_col.columns.values) + list(features_X_col_tree)  ))
+    #TODO remove columns that have more than 0.9 correlation between them, as they are redundant. More info help: https://machinelearningmastery.com/basic-data-cleaning-for-machine-learning/
+    columns_json_BOTH = list(set(list(features_X_col_corr) + list(features_X_col_tree)  ))
     Feature_selection_create_json.created_json_feature_selection(list_all_columns=columns_json_BOTH,path_json=f'data/columns_select/{symbol}_{REF_MODEL}_corr.json')
     features_X = features_X_ALL[columns_json_BOTH]
     print("[4.2] Select the best technical patterns to train with features_W3 Extracted Count: ", len(features_X.columns), " Names : ",",".join(features_X.columns))
@@ -141,29 +160,32 @@ for symbol in stocks_list : #stocks_list:
     model = keras.Sequential([
         keras.layers.Dense(WINDOWS_SIZE*3, activation=ACTIVATION_2_ReLU, input_shape = input_shape ),
         keras.layers.Dense(72, activation=ACTIVATION_2_ReLU),
-        keras.layers.Dropout(0.1),
-        keras.layers.Dense(36, activation=ACTIVATION_2_ReLU),
-        keras.layers.Dropout(0.1),
+        keras.layers.Dropout(0.32),
+        keras.layers.Dense(64, activation=ACTIVATION_2_ReLU),
+        keras.layers.Dense(64, activation=ACTIVATION_2_ReLU),
+        keras.layers.Dropout(0.4),
         keras.layers.Dense(12, activation=ACTIVATION_2_ReLU),
+        keras.layers.Dropout(0.1),
         keras.layers.Flatten(),
         keras.layers.Dense(output_shape, activation='softmax')#, bias_initializer = output_bias_k),
     ])
-    #TIP must respect the input_shape and output_shape and the 'softmax' , he rest can be edited for EXPRIMENT combinations
-    model.compile(loss=tf.keras.losses.CategoricalCrossentropy(), optimizer=Adam(lr=0.001), metrics=tf.keras.metrics.CategoricalAccuracy())
-    print("[9.2] optimicer config : ", model.optimizer.get_config())
+    # TIP inspired by the kaggle winner  https://www.kaggle.com/code/viktortaran/ps-s-3-e-6#%E2%9C%855.12.-Keras
+    loss = keras.losses.MeanSquaredError(reduction="auto", name="mean_squared_error")
+    # reduce_lr_loss = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_acc', factor=0.8, patience=6, verbose=1,min_delta=0.005, min_lr=0.000001, mode='max')
+    # early_stopping = keras.callbacks.EarlyStopping(patience=12, min_delta=0.0001, monitor="val_acc", mode='max',restore_best_weights=True)
+    reduce_lr_loss = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.8, patience=5, verbose=1,min_delta=0.001, min_lr=0.000001, mode='min')
+    early_stopping = keras.callbacks.EarlyStopping(patience=11, monitor="val_loss", mode='min', verbose=1,restore_best_weights=True)
+    callbacks_save = ModelCheckpoint(filepath=f'outputs/{symbol}_{REF_MODEL}_buysell.h5',monitor='val_loss', verbose=1, save_best_only=True)
     tf.keras.utils.plot_model(model, to_file=f'outputs/plots/{symbol}_{REF_MODEL}_buysell.png')
     print("[9.3] print diagram of the TF model Path: ", f'outputs/plots/{symbol}_{REF_MODEL}_buysell.png')
 
     # [10] Trian the model
     print("\n[10] Start training TF model "+ bcolors.OKBLUE  , f'outputs/{symbol}_{REF_MODEL}_buysell.h5' , bcolors.ENDC)
-    # with tf.device('/cpu:0'): # to improve the eficent https://stackoverflow.com/questions/62916904/failed-copying-input-tensor-from-cpu-to-gpu-in-order-to-run-gatherve-dst-tensor
-    #     X_train = tf.convert_to_tensor(X_train, np.float32); y_train = tf.convert_to_tensor(y_train, np.float32)
-    model_history = model.fit(
-          x=X_train, y=y_train , batch_size=16, validation_split=0.33, epochs=90,class_weight=dict_class_weight,
-        callbacks=[
-            ModelCheckpoint(filepath=f'outputs/{symbol}_{REF_MODEL}_buysell.h5', monitor='val_loss', save_best_only=True),
-            EarlyStopping(monitor='val_loss', patience=10, verbose=2)
-        ],   verbose=1)
+    print("[10.1] To adjust and measure the model, callbacks is required, the callbacks will stop before the overfit. ")
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0005 ), loss=loss, metrics=get_metrics(num_classes=len(target_Y.columns)))
+    # TIP f1 is the BEST metric to unbalance cases
+    history = model.fit(x=X_train, y=y_train, batch_size=128, epochs=280, class_weight=dict_class_weight, verbose=1,
+                        validation_split=0.33, callbacks=[reduce_lr_loss, early_stopping, callbacks_save])
     # TIP on a monitor= you can stop training when you get the best precision    recall  f1-score , but the one that works best is val_loss.
     print("[10.1] Model initial_weights saved : ",  f'data/initial_weights/{symbol}_{REF_MODEL}_initial_weights.tf')
     model.save_weights( f'data/initial_weights/{symbol}_{REF_MODEL}_initial_weights.tf')
@@ -188,5 +210,4 @@ for symbol in stocks_list : #stocks_list:
     gc.collect()
     print("\n")
 print("END")
-
 
